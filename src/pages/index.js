1b210420b309afdabaa29a8bf688b221573c60b4
Bhavyesh <bhavyesh@Bhavyeshs-MacBook-Air.local>
2023-11-19 22:01:13 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Form, Layout } from "@/components";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Comicon</title>
+        <meta name="description" content="Best Comic Generator" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.jpeg" />
+      </Head>
+      <Layout>
+        <Form />
+      </Layout>
+    </>
+  );
+}
